8a38bf145f33fe3eff6d0a098d63259e1f3c4b3b
Kayzer <max.mlv2210@gmail.com>
2024-05-07 17:15:52 +0200

    Switch img

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i - 1;
         }
       });
       next =
@@ -193,7 +193,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          index = i + 1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
